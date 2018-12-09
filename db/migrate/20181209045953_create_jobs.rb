2649b0dda96360ef1c73cd59d29116b51a77948f
Elias Becker <Shobecker@gmail.com>
2018-12-09 00:00:43 -0500

    add jobs

--- db/migrate/20181209045953_create_jobs.rb
+++ db/migrate/20181209045953_create_jobs.rb
@@ -1,11 +1,14 @@
 class CreateJobs < ActiveRecord::Migration[5.1]
   def change
     create_table :jobs do |t|
-      t.text :content
+      t.integer :bedrooms
+      t.text :fromAdress
+      t.text :toAddress
       t.references :customer, foreign_key: true
+      t.boolean :active
+      t.text :worker
 
       t.timestamps
     end
-    add_index :jobs, [:customer_id, :created_at]
   end
 end
