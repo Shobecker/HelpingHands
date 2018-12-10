20032b640aa6bb7c76a98158003ab0d86815536c
Elias Becker <Shobecker@gmail.com>
2018-12-09 23:26:13 -0500

    jobs default active = false

--- db/migrate/20181209161151_create_jobs.rb
+++ db/migrate/20181209161151_create_jobs.rb
@@ -5,7 +5,7 @@ class CreateJobs < ActiveRecord::Migration[5.1]
       t.text :fromAdress
       t.text :toAddress
       t.references :customer, foreign_key: true
-      t.boolean :active, default: true
+      t.boolean :active, default: false
       t.text :worker
 
       t.timestamps
